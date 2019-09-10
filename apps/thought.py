e3525bf553fd009617f50afffecd9563d6fa12dd
Patrick <nianqinianyi@163.com>
2019-09-10 08:53:11 +0800

    点赞数量、查看数量

--- apps/thought.py
+++ apps/thought.py
@@ -10,9 +10,9 @@ PURPOSE.
 See the Mulan PSL v1 for more details.
 """
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, jsonify
 
-from config.db import select_one, select_list
+from config.db import select_one, select_list, run_sql2
 from config.utils import is_login
 
 app_thought = Blueprint('app_thought', __name__)
@@ -25,6 +25,12 @@ def thought_index():
 
 @app_thought.route("/<int:id>")
 def thought(id):
+    run_sql2('''
+    update app_posts
+    set view_count = view_count + 1
+    where id = ?
+    ''', (id,))
+
     app_posts = select_one('''
                 select * 
                 from app_posts 
@@ -42,6 +48,24 @@ def thought(id):
                   )
         ''' % app_posts['id'])
 
-
-
     return render_template('thoughts/thought.html', app_posts=app_posts, app_tags=app_tags, is_login=is_login())
+
+
+@app_thought.route("/thumb/<int:post_id>", methods=['POST'])
+def thumb(post_id):
+    run_sql2('''
+    update app_posts
+    set thumb_count = thumb_count + 1
+    where id = ?
+    ''', (post_id,))
+
+    app_posts = select_one('''
+                select * 
+                from app_posts 
+                where id = %d
+            ''' % post_id)
+
+    return jsonify({
+        "success":True,
+        "data": app_posts['thumb_count']
+    })
