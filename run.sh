f4a4e205af4fad56bcd7f5dc42bb9612f755f4d1
Patrick <nianqinianyi@163.com>
2019-09-26 22:07:53 +0800

    脚本

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+cmd=$(ps aux|grep sixlab_site|grep python|awk '{print $2}')
+
+echo -e "PID: \n\033[31m\033[05m$cmd\033[0m"
+echo "PID:$cmd"
+
+for id in ${cmd}
+    do
+    kill -9 ${id}
+    echo "kill $id"
+    done
+
+echo 'finish kill'
 
 source venv/bin/activate
 
